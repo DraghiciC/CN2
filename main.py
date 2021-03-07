ea947faf056a5eed430b138de4777ae6c6710809
DraghiciC <draghici.constantin.danut@gmail.com>
2021-03-07 16:40:47 +0200

    First 4 points Signed-off-by: DraghiciC <draghici.constantin.danut@gmail.com>

--- main.py
+++ main.py
@@ -47,6 +47,48 @@ def Determinant(A,n):
         det*=A[i][i]
     det*=det #calcul determinant A
     return det
+def Calcul_X(A,n,b):
+    print("Calculating X")
+    x=[0 for i in range(n)]
+    for i in range(n):
+        aux=0
+        for j in range(i):
+            aux+=x[j]*A[i][j]
+        x[i]=(b[i]-aux)/A[i][i]
+    y = [0 for i in range(n)]
+    for i in range(n):
+        aux=0
+        for j in range(i):
+            aux+=y[n-j-1]*A[n-j-1][n-i-1]
+        y[n-i-1]=(x[n-i-1]-aux)/A[n-i-1][n-i-1]
+    print("x=",y)
+    return y
+def Calc_Norm(A,n,X,diagonal,b):
+    print("testing result")
+    result = [0 for i in range(n)]
+    print(diagonal)
+    for a in range(n):
+        print(A[a])
+    for i in range(n):
+        aux=0
+        for j in range(n):
+            if i==j:
+                aux+=diagonal[i]*X[j]
+            elif j<i:
+                aux+=X[j]*A[j][i]
+            else:
+                aux+=X[j]*A[i][j]
+        result[i]=aux
+    print("result=",result)
+    print("b=",b)
+    norm=0
+    for i in range(n):
+        norm+=pow(result[i]-b[i],2)
+    norm=math.sqrt(norm)
+    print("norm = ",norm)
+
 A=Cholesky_Decomposition(A, n)
 d=Determinant(A,n)
+X=Calcul_X(A,n,b)
+test=Calc_Norm(A,n,X,diagonal,b)
 
